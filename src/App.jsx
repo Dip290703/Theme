49ddb47db6690761cdc45d7b9e36f032bc449626
Dip290703 <dipanshu6233@gmail.com>
2025-05-24 11:45:37 +0530

    change button

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
     <div className= {theme === "light" ? "bg-white text-black h-screen" : "bg-black h-screen text-white"}>
      <div className="flex items-center justify-center h-full">
        <button
-       className="px-4 py-2 bg-blue-500 text-white rounded" 
+       className="px-4 py-2 border rounded font-bold" 
        onClick={changeTheme}>
         {
           theme === "light" ? "Switch to Dark Mode" : "Switch to Light Mode"

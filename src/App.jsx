d04fb2e6ff24fbbc60f00780b2618c6ea2d5aa09
Dip290703 <dipanshu6233@gmail.com>
2025-05-24 11:24:24 +0530

    theme

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const App = () => {
+  const [theme, setTheme] = React.useState("light");
+  const changeTheme = () => {
+    setTheme((prevTheme) => {
+      return prevTheme === "light" ? "dark" : "light";
+    });
+  };
+  return (
+    <div className= {theme === "light" ? "bg-white text-black h-screen" : "bg-black h-screen text-white"}>
+     <div className="flex items-center justify-center h-full">
+       <button
+       className="px-4 py-2 bg-blue-500 text-white rounded" 
+       onClick={changeTheme}>
+        {
+          theme === "light" ? "Switch to Dark Mode" : "Switch to Light Mode"
+        }
+       </button>
+     </div>
+    </div>
+  );
+};
+
+export default App;
